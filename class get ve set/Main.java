7c02fe1aa62753320c11a32941a5515d0c4d441d
Emre KAZAZ <80270457+emrekazaz-hub@users.noreply.github.com>
2021-05-19 17:26:29 +0300

    get / set
    class get ve set

---
+++ class get ve set/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Product product1 = new Product(1,"Lenovo V14", 15000, "16 gb ram",10,13500);
+
+        Product product2 = new Product();
+        product2.setId(2);
+        product2.setName("Lenovo V15");
+        product2.setDetail("32 gb ram");
+        product2.setDiscount(10);
+        product2.setUnitPrice(16000);
+        System.out.println(product2.getGetUnitPriceAfterDiscount());
+
+        Category category1 = new Category();
+        category1.setId(1);
+        category1.setName("icecek");
+
+        Category category2 = new Category();
+        category2.setId(2);
+        category2.setName("yiyecek");
+
+        System.out.println("kategori ismi : "+ category1.getName());
+        System.out.println("kategori ismi : "+ category2.getName());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
